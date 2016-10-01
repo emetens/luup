84ba6e0922224cee9c9c657ccedfe2961d379c71
Eric Metens <em@bolt.com>
2016-10-01 15:31:37 -0700

    split in progress

---
+++ plugin/L_SenseMe/plugin.lua
@@ -0,0 +1,10 @@
+local VERSION = "0.10"
+
+local PLUGIN = {
+  -- PLUGIN_ID = 8588,
+  NAME = "SenseMe Gateway",
+  MIOS_VERSION = "unknown",
+  DEBUG_MODE = false,
+  PLUGIN_DISABLED = false,
+  FILES_VALIDATED = false,
+}
